d351ee96fd9d3df032846bb17414916f01f5473b
OSwego <syava23223@gmail.com>
2025-03-21 14:33:29 +0200

    add maven dependencies

--- src/main/java/com/example/springjdbccustomer/controller/AppController.java
+++ src/main/java/com/example/springjdbccustomer/controller/AppController.java
@@ -3,6 +3,7 @@ package com.example.springjdbccustomer.controller;
 import com.example.springjdbccustomer.dto.*;
 import com.example.springjdbccustomer.model.Customer;
 import com.example.springjdbccustomer.service.CustomerService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,39 +22,39 @@ public class AppController {
     public ResponseEntity<CustomerDtoCreateResponse> addCustomer(@RequestBody CustomerDtoRequest request) {
         Customer customer = customerService.addCustomer(request);
         return (customer != null) ?
-                ResponseEntity.ok().body(CustomerDtoCreateResponse.of(true, customer)) :
-                ResponseEntity.ok().body(CustomerDtoCreateResponse.of(false, null));
+                ResponseEntity.status(HttpStatus.OK).body(CustomerDtoCreateResponse.of(true, customer)) :
+                ResponseEntity.status(HttpStatus.OK).body(CustomerDtoCreateResponse.of(false, null));
     }
 
     @GetMapping
     public ResponseEntity<CustomerDtoListResponse> getAllCustomers() {
         List<Customer> customerList = customerService.fetchAllCustomers();
         return (customerList != null) ?
-                ResponseEntity.ok().body(CustomerDtoListResponse.of(true, customerList)) :
-                ResponseEntity.ok().body(CustomerDtoListResponse.of(false, null));
+                ResponseEntity.status(HttpStatus.OK).body(CustomerDtoListResponse.of(true, customerList)) :
+                ResponseEntity.status(HttpStatus.OK).body(CustomerDtoListResponse.of(false, null));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<CustomerDtoDeleteResponse> deleteCustomerById(@PathVariable Long id) {
         return (customerService.deleteCustomerById(id)) ?
-                ResponseEntity.ok().body(CustomerDtoDeleteResponse.of(true, id)) :
-                ResponseEntity.ok().body(CustomerDtoDeleteResponse.of(false, id));
+                ResponseEntity.status(HttpStatus.OK).body(CustomerDtoDeleteResponse.of(true, id)) :
+                ResponseEntity.status(HttpStatus.OK).body(CustomerDtoDeleteResponse.of(false, id));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<CustomerDtoGetByIdResponse> getCustomerById(@PathVariable Long id) {
         Customer customer = customerService.fetchCustomerById(id);
         return (customer != null) ?
-                ResponseEntity.ok().body(CustomerDtoGetByIdResponse.of(true, id, customer)) :
-                ResponseEntity.ok().body(CustomerDtoGetByIdResponse.of(false, id, null));
+                ResponseEntity.status(HttpStatus.OK).body(CustomerDtoGetByIdResponse.of(true, id, customer)) :
+                ResponseEntity.status(HttpStatus.OK).body(CustomerDtoGetByIdResponse.of(false, id, null));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<CustomerDtoUpdateResponse> updateCustomer(@PathVariable Long id, @RequestBody CustomerDtoRequest request) {
         if (customerService.fetchCustomerById(id) != null) {
-            return ResponseEntity.ok().body(CustomerDtoUpdateResponse.of(true, id, customerService.updateCustomerById(id, request)));
+            return ResponseEntity.status(HttpStatus.OK).body(CustomerDtoUpdateResponse.of(true, id, customerService.updateCustomerById(id, request)));
         } else {
-            return ResponseEntity.ok().body(CustomerDtoUpdateResponse.of(false, id, null));
+            return ResponseEntity.status(HttpStatus.OK).body(CustomerDtoUpdateResponse.of(false, id, null));
         }
     }
 
@@ -61,15 +62,15 @@ public class AppController {
     public ResponseEntity<CustomerDtoGetByEmailResponse> getCustomerByEmail(@PathVariable String email) {
         Customer customer = customerService.fetchCustomerByEmail(email);
         return (customer != null) ?
-                ResponseEntity.ok().body(CustomerDtoGetByEmailResponse.of(true, email)) :
-                ResponseEntity.ok().body(CustomerDtoGetByEmailResponse.of(false, null));
+                ResponseEntity.status(HttpStatus.OK).body(CustomerDtoGetByEmailResponse.of(true, email)) :
+                ResponseEntity.status(HttpStatus.OK).body(CustomerDtoGetByEmailResponse.of(false, null));
     }
 
     @GetMapping("/{ssn}")
     public ResponseEntity<CustomerDtoGetBySocialNumberResponse> getCustomerBySsn(@PathVariable String ssn) {
         Customer customer = customerService.fetchCustomerBySocialSecurityNumber(ssn);
         return (customer != null) ?
-                ResponseEntity.ok().body(CustomerDtoGetBySocialNumberResponse.of(true, ssn)) :
-                ResponseEntity.ok().body(CustomerDtoGetBySocialNumberResponse.of(false, null));
+                ResponseEntity.status(HttpStatus.OK).body(CustomerDtoGetBySocialNumberResponse.of(true, ssn)) :
+                ResponseEntity.status(HttpStatus.OK).body(CustomerDtoGetBySocialNumberResponse.of(false, null));
     }
 }
